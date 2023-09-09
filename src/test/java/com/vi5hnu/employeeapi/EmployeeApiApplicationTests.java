d84d2ef07cf82181c4be898949faefb1afdf7437
vishnukumar <vishnu.kumar@digio.in>
2023-09-09 10:15:20 +0530

    initial commit

---
+++ src/test/java/com/vi5hnu/employeeapi/EmployeeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vi5hnu.employeeapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
